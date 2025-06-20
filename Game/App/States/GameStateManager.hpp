6bd2b030db90afd6f8a4e62782d7937eac6260b2
Gleb Melnik <339891@niuitmo.ru>
2025-06-20 17:58:26 +0300

    Enhance GameStateManager with templates and concepts
    - Add `GameStateDerived` concept for improved type safety.
    - Refactor `PushState` and introduce `ReplaceState` template methods.
    - Add `[[nodiscard]]` annotations to relevant methods.

--- Game/App/States/GameStateManager.hpp
+++ Game/App/States/GameStateManager.hpp
@@ -10,6 +10,9 @@ namespace Radon
 {
 using namespace Urho3D;
 
+template <typename T>
+concept GameStateDerived = std::derived_from<T, GameState>;
+
 class GameStateManager final : public Object
 {
     URHO3D_OBJECT(GameStateManager, Object);
@@ -20,22 +23,17 @@ public:
 
     void Update(float timeStep);
 
-    template <typename T, typename... Args>
-    void PushState(Args&&... args)
-    {
-        if (!states_.empty())
-            states_.top()->Exit();
+    template <typename GameStateDerived, typename... Args>
+    void PushState(Args&&... args);
 
-        auto newState = MakeShared<T>(context_, std::forward<Args>(args)...);
-        states_.push(newState);
-        newState->Enter();
-    }
+    template <typename GameStateDerived, typename... Args>
+    void ReplaceState(Args&&... args);
 
     void PopState();
     void ClearStates();
 
-    bool HasStates() const { return !states_.empty(); }
-    SharedPtr<GameState> CurrentState() const { return states_.empty() ? nullptr : states_.top(); }
+    [[nodiscard]] bool HasStates() const { return !states_.empty(); }
+    [[nodiscard]] SharedPtr<GameState> CurrentState() const { return states_.empty() ? nullptr : states_.top(); }
 
 private:
     std::stack<SharedPtr<GameState>> states_;
