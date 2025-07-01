700e807269009a60c417aeb7136106f1b2bd917a
Gleb Melnik <339891@niuitmo.ru>
2025-07-01 15:53:28 +0300

    Add component registration and InputHandler subsystem
    - Introduce `RegisterComponents` method in `RadonApplication` for modular component registration.
    - Add `InputHandler` subsystem to improve input processing.
    - Update `RegisterSubsystems` to include the new subsystem.

--- Source/App/RadonApplication.cpp
+++ Source/App/RadonApplication.cpp
@@ -4,9 +4,11 @@
 #include "Engine/Core/Logger.hpp"
 #include "Engine/Graphics/ViewportManager.hpp"
 #include "Engine/Input/CameraManager.hpp"
+#include "Engine/Input/InputHandler.hpp"
 #include "Engine/Scene/SceneManager.hpp"
 #include "Engine/StateMachine/GameStateManager.hpp"
 #include "Engine/UI/UIManager.hpp"
+#include "Game/Components/RegisterComponents.hpp"
 #include "Game/States/MenuState.hpp"
 
 #include <Urho3D/Engine/EngineDefs.h>
@@ -30,6 +32,7 @@ void RadonApplication::Setup()
 void RadonApplication::Start()
 {
     RADON_LOGINFO("RadonApplication: Start called");
+    RegisterComponents();
     RegisterSubsystems();
     GetSubsystem<Engine::StateMachine::GameStateManager>()->PushState(MakeShared<Game::States::MenuState>(context_));
 }
@@ -39,15 +42,22 @@ void RadonApplication::Stop()
     RADON_LOGINFO("RadonApplication: Stop called");
 }
 
+void RadonApplication::RegisterComponents()
+{
+    using namespace Game::Components;
+    RegisterPlayerComponents(context_);
+}
+
 void RadonApplication::RegisterSubsystems()
 {
     using namespace Engine;
 
     RADON_LOGDEBUG("RadonApplication: RegisterSubsystems called");
-    context_->RegisterSubsystem(MakeShared<Scene::SceneManager>(context_));
     context_->RegisterSubsystem(MakeShared<StateMachine::GameStateManager>(context_));
-    context_->RegisterSubsystem(MakeShared<UI::UIManager>(context_));
-    context_->RegisterSubsystem(MakeShared<Input::CameraManager>(context_));
+    context_->RegisterSubsystem(MakeShared<Scene::SceneManager>(context_));
     context_->RegisterSubsystem(MakeShared<Graphics::ViewportManager>(context_));
+    context_->RegisterSubsystem(MakeShared<Input::CameraManager>(context_));
+    context_->RegisterSubsystem(MakeShared<Input::InputHandler>(context_));
+    context_->RegisterSubsystem(MakeShared<UI::UIManager>(context_));
     RADON_LOGINFO("RadonApplication: All subsystems registered");
 }
--- Source/App/RadonApplication.hpp
+++ Source/App/RadonApplication.hpp
@@ -15,6 +15,7 @@ public:
     void Stop() override;
 
 private:
+    void RegisterComponents();
     void RegisterSubsystems();
 };
 
